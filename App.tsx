38c941c38342dfd3885ceb5e28fdc697b80b4ce1
DamasoGE <franciscodamasogimenez@gmail.com>
2025-10-07 18:30:44 +0200

    cabecera finalizada

--- App.tsx
+++ App.tsx
@@ -1,13 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, View } from 'react-native'
 import React from 'react'
+import { Image } from 'expo-image'
 
 export default function App() {
+
+  const COLOR_FONDO="#121212"
+  const COLOR_TITULO="#ffdd99"
+  const COLOR_TEXTO_FOTO="#ffffff"
+
   return (
-    <View>
-
-
+    <View style={[styles.contenedorPrincipal, {backgroundColor:COLOR_FONDO}]}>
+      <ScrollView>  
+        <Image source={require('./assets/images/granada_light.jpg')}
+          style={{width:"100%", height:250, resizeMode:"cover"}} 
+        />
+      </ScrollView>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  contenedorPrincipal:{
+    flex:1,
+  },
+
+})
